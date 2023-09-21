06386d6aab61ed336f9e6923b86b3555d3cd462b
wuliten <bryandborst@gmail.com>
2023-09-21 19:22:04 -0400

    week 2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumver = -1;
+        while (userNumver !=0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string userResponse = Console.ReadLine();
+            userNumver = int.Parse(userResponse);
+            if (userNumver != 0)
+            {
+                numbers.Add(userNumver);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
